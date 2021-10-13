6523b9a5cc363fca7d4d8993dafd9d4df3e3fae6
Heechang Kim <kim@LAPTOP-GLU1554K.localdomain>
2021-10-14 01:21:19 +0900

    Initial commit

---
+++ wav2vec2/data.py
@@ -0,0 +1,11 @@
+from torch.utils.data import Dataset
+import torch
+import numpy as np
+import librosa
+
+def prepare_dataset():
+    data = ...
+    data = data.map()
+    ...
+    
+    return data
---
+++ wav2vec2/setup.py
@@ -0,0 +1,13 @@
+#nsml: dacon/nia-pytorch:1.0
+from distutils.core import setup
+
+setup(
+    name='ladder_networks',
+    version='1.0',
+    install_requires=[
+        'datasets==1.8.0',
+        'transformers==4.4.0',
+        'soundfile',
+        'jiwer'
+    ]
+)
