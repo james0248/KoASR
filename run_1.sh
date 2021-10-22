dfccb3d8f9093b199d871da63e535abfedf214e9
james0248 <hsjames0248@gmail.com>
2021-10-22 15:49:04 +0900

    Update training args

--- run_1.sh
+++ run_1.sh
@@ -4,9 +4,9 @@ nsml run -e ./wav2vec2/new_main.py -d stt_1 \
     -c 8 -g 1 --memory 70G --shm-size 13G -a \
     "--output_dir ./wav2vec2-korean-1
     --model_name_or_path facebook/wav2vec2-large-xlsr-53 
-    --num_train_epochs 2
-    --per_device_train_batch_size 4
-    --per_device_eval_batch_size 4
+    --num_train_epochs 5
+    --per_device_train_batch_size 8
+    --per_device_eval_batch_size 8
     --evaluation_strategy steps 
     --eval_steps 300 
     --save_strategy no
@@ -18,7 +18,5 @@ nsml run -e ./wav2vec2/new_main.py -d stt_1 \
     --group_by_length False
     --freeze_feature_extractor True 
     --mode train
-    --split 0
-    --max_split 1
     --gradient_accumulation_steps 10
     --eval_accumulation_steps 500" 
