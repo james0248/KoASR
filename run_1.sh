ff15d8f8bb561ad64aa120227d5776c49b1bd989
james0248 <hsjames0248@gmail.com>
2021-11-27 17:40:34 +0900

    Change submit commands

--- run_1.sh
+++ run_1.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 sleep 0m # Be patient!
 nsml run -e ./wav2vec2/main.py -d final_stt_1 \
-    -c 8 -g 1 --memory 50G --shm-size 13G -a \
+    -c 8 -g 1 --memory 80G --shm-size 2G -a \
     "--output_dir ./wav2vec2-korean-1
     --model_name_or_path facebook/wav2vec2-large-xlsr-53 
-    --num_train_epochs 5
+    --num_train_epochs 10
     --per_device_train_batch_size 6
     --per_device_eval_batch_size 6
-    --evaluation_strategy steps 
-    --eval_steps 1000
+    --gradient_accumulation_steps 20
+    --evaluation_strategy epoch
     --save_strategy no
     --save_total_limit 2 
-    --learning_rate 3e-5
-    --warmup_steps 0 
+    --learning_rate 1e-5
+    --warmup_steps 20 
     --attention_dropout 0.094
     --activation_dropout 0.055
     --feat_proj_dropout 0.04
@@ -25,12 +25,12 @@ nsml run -e ./wav2vec2/main.py -d final_stt_1 \
     --group_by_length True
     --freeze_feature_extractor True 
     --mode train
-    --gradient_accumulation_steps 1
     --eval_accumulation_steps 500
     --writer_batch_size 500
-    --max_split 10
+    --cpu_batch_size 10000
+    --max_split 1
     --split 0
     --data_type 2
     --disable_tqdm True
-    --logging_steps 50
+    --logging_steps 5
     " 
